9afec3f5aec914c8f0cd53b574bbb26838fc2b3a
Tung Nguyen <tunginobi@gmail.com>
2009-07-29 15:28:04 +1000

    Check for bad input for point, rect, circle.
     * point raises if the point is out-of-bounds.
     * rect raises if the width or height is less than 0.
     * circle raises if the radius is less than 0.

--- lib/tea/m_primitive_drawing.rb
+++ lib/tea/m_primitive_drawing.rb
@@ -27,13 +27,25 @@ module Tea
     end
 
     # Plot a point at (x, y) with the given color (0xRRGGBBAA) on the Bitmap.
+    #
+    # Raises Tea::Error if (x, y) is outside of the drawing buffer.
     def point(x, y, color)
+      w = primitive_buffer.w
+      h = primitive_buffer.h
+      if x < 0 || x > w || y < 0 || y > h
+        raise Tea::Error, "can't plot point (#{x}, #{y}), not within #{w}x#{h}", caller
+      end
       primitive_buffer[x, y] = primitive_color(color)
     end
 
     # Draw a rectangle of size w * h with the top-left corner at (x, y) with
     # the given color (0xRRGGBBAA).
+    #
+    # Raises Tea::Error if w or h are less than 0.
     def rect(x, y, w, h, color)
+      if w < 0 || h < 0
+        raise Tea::Error, "can't draw rectangle of size #{w}x#{h}", caller
+      end
       primitive_buffer.fill_rect x, y, w, h, primitive_color(color)
     end
 
@@ -53,7 +65,13 @@ module Tea
     # +:outline+::    If true, do not fill the circle, just draw an outline.
     # +:antialias+::  If true, smooth the edges of the circle with
     #                 antialiasing.
+    #
+    # Raises Tea::Error if the radius is less than 0.
     def circle(x, y, radius, color, options=nil)
+      if radius < 0
+        raise Tea::Error, "can't draw circle with radius #{radius}", caller
+      end
+
       if options
         primitive_buffer.draw_circle x, y, radius, primitive_color(color),
                                      !options[:outline], options[:antialias]
