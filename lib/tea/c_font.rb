f7026422f56717a874bbaad3f6a6e05da5bb0995
Tung Nguyen <tunginobi@gmail.com>
2009-10-10 11:00:27 +1100

    Major refactoring of font code.
     * Unfortunately, this theoretically makes font drawing slower.

--- lib/tea/c_font.rb
+++ lib/tea/c_font.rb
@@ -46,10 +46,13 @@ module Tea
     BITMAP_FONT = :BITMAP_FONT
     SFONT = :SFONT
 
-    BITMAP_FONT_ALPHABET_LENGTH = 256
+    BITMAP_FONT_START = 0
+    BITMAP_FONT_LENGTH = 256
+    SFONT_START = 33
+    SFONT_LENGTH = 94
+    CODE_POINT_SPACE = 32
+
     SFONT_ALPHABET_LENGTH = 94
-    SFONT_ALPHABET_OFFSET = 33
-    ASCII_SPACE = 32
 
     # Create a new font from a font file given by +path+.
     #
@@ -65,20 +68,14 @@ module Tea
     # May raise Tea::Error on failure.
     def initialize(path, font_type, options=nil)
       font = SDL::Surface.load(path)
-
-      if options && options.has_key?(:transparent_color)
-        @transparent_color = Color.split(options[:transparent_color])
-      end
-
+      transparent_color = options ? options[:transparent_color] : nil
       @font_type = font_type
 
       case font_type
       when BITMAP_FONT
-        @glyphs = letters_from_bitmap_font(font)
-        @alphabet_offset = 0
+        @glyphs = letters_from_bitmap_font(font, transparent_color)
       when SFONT
-        @glyphs = letters_from_sfont(font)
-        @alphabet_offset = SFONT_ALPHABET_OFFSET
+        @glyphs = letters_from_sfont(font, transparent_color)
       end
     rescue SDL::Error => e
       raise Tea::Error, e.message, e.backtrace
@@ -87,40 +84,7 @@ module Tea
     # Calculate the pixel width of a string rendered with this font.
     def string_w(string)
       w = 0
-      if ruby_version_match = RUBY_VERSION.match(/(\d+)\.(\d+)\.\d+/)
-        ruby_major = ruby_version_match[1].to_i
-        ruby_minor = ruby_version_match[2].to_i
-        if ruby_major >= 1 && ruby_minor >= 9
-          case @font_type
-          when BITMAP_FONT
-            string.codepoints { |pt| w += @glyphs[pt - @alphabet_offset].w }
-          when SFONT
-            string.codepoints do |pt|
-              if pt == ASCII_SPACE
-                # Fudge the width of a space.
-                w += @glyphs[0].w
-              else
-                w += @glyphs[pt - @alphabet_offset].w
-              end
-            end
-          end
-        else
-          case @font_type
-          when BITMAP_FONT
-            string.length.times { |i| w += @glyphs[string[i] - @alphabet_offset].w }
-          when SFONT
-            string.length.times do |i|
-              pt = string[i]
-              if pt == ASCII_SPACE
-                # Fudge the width of a space.
-                w += @glyphs[0].w
-              else
-                w += @glyphs[pt - @alphabet_offset].w
-              end
-            end
-          end
-        end
-      end
+      each_code_point(string) { |pt| w += get_glyph_w(pt) }
       w
     end
 
@@ -133,119 +97,123 @@ module Tea
     # Write the given string onto the bitmap at (x, y).
     def draw_to(bitmap, x, y, string)
       draw_x = x
-      if ruby_version_match = RUBY_VERSION.match(/(\d+)\.(\d+)\.\d+/)
-        ruby_major = ruby_version_match[1].to_i
-        ruby_minor = ruby_version_match[2].to_i
-        if ruby_major >= 1 && ruby_minor >= 9
-          case @font_type
-          when BITMAP_FONT
-            string.codepoints do |pt|
-              g = @glyphs[pt - @alphabet_offset]
-              bitmap.blit g, draw_x, y
-              draw_x += g.w
-            end
-          when SFONT
-            string.codepoints do |pt|
-              if pt == ASCII_SPACE
-                # Use the first char for the space width.
-                draw_x += @glyphs[0].w
-              else
-                g = @glyphs[pt - @alphabet_offset]
-                bitmap.blit g, draw_x, y
-                draw_x += g.w
-              end
-            end
-          end
+      each_code_point(string) do |pt|
+        glyph = get_glyph(pt)
+        if glyph
+          bitmap.blit glyph, draw_x, y
+          draw_x += glyph.w
         else
-          case @font_type
-          when BITMAP_FONT
-            string.length.times do |i|
-              g = @glyphs[string[i] - @alphabet_offset]
-              bitmap.blit g, draw_x, y
-              draw_x += g.w
-            end
-          when SFONT
-            string.length.times do |i|
-              pt = string[i]
-              if pt == ASCII_SPACE
-                # Use the first char for the space width.
-                draw_x += @glyphs[0].w
-              else
-                g = @glyphs[string[i] - @alphabet_offset]
-                bitmap.blit g, draw_x, y
-                draw_x += g.w
-              end
-            end
-          end
+          draw_x += get_glyph_w(pt)
         end
       end
+
       string
     end
 
     private
 
     # Extract an array of letter glyph Bitmaps from a bitmap font.
-    def letters_from_bitmap_font(font_surface)
-      unless font_surface.w % BITMAP_FONT_ALPHABET_LENGTH == 0
-        raise Tea::Error, "Bitmap font cannot be evenly divided into 256 glyphs (w == #{font_surface.w})", caller
+    def letters_from_bitmap_font(font_surface, transparent_color)
+      unless font_surface.w % BITMAP_FONT_LENGTH == 0
+        raise Tea::Error, "Bitmap font cannot be evenly divided into #{BITMAP_FONT_LENGTH} glyphs (w == #{font_surface.w})", caller
       end
 
-      char_w = font_surface.w / BITMAP_FONT_ALPHABET_LENGTH
+      char_w = font_surface.w / BITMAP_FONT_LENGTH
       char_h = font_surface.h
+      trans_rgba = transparent_color ? Color.split(transparent_color) : nil
 
-      glyphs = if @transparent_color
-                 Array.new(BITMAP_FONT_ALPHABET_LENGTH) do |i|
-                  g = Bitmap.new(char_w, char_h, Color::CLEAR)
-                  for gy in 0...char_h
-                    for gx in 0...char_w
-                      c = font_surface.get_rgba(font_surface[char_w * i + gx, gy])
-                      next if c == @transparent_color
-                      g[gx, gy] = Color.mix(*c)
-                    end
-                  end
-                  g
-                end
-              else
-                Array.new(BITMAP_FONT_ALPHABET_LENGTH) do |i|
-                  g = Bitmap.new(char_w, char_h, Color::CLEAR)
-                  for gy in 0...char_h
-                    for gx in 0...char_w
-                      g[gx, gy] = Color.mix(*font_surface.get_rgba(font_surface[char_w * i + gx, gy]))
-                    end
-                  end
-                  g
-                end
-              end
+      glyphs = Array.new(BITMAP_FONT_LENGTH) do |char|
+        glyph = Bitmap.new(char_w, char_h, Color::CLEAR)
+        for gy in 0...char_h
+          for gx in 0...char_w
+            color = font_surface.get_rgba(font_surface[char_w * char + gx, gy])
+            glyph[gx, gy] = Color.mix(*color) unless color == trans_rgba
+          end
+        end
+        glyph
+      end
 
       glyphs
     end
 
     # Extract an array of letter glyph Bitmaps from an SFont.
-    def letters_from_sfont(font_surface)
+    def letters_from_sfont(font_surface, transparent_color)
       detect_x = 0
-      glyphs = Array.new(SFONT_ALPHABET_LENGTH) do |i|
+      glyphs = Array.new(SFONT_LENGTH) do |i|
         detect_x += 1 while Tea::Color.mix(*font_surface.get_rgba(font_surface[detect_x, 0])) == Tea::Color::MAGENTA && detect_x < font_surface.w
-        raise Tea::Error, "Expected #{SFONT_ALPHABET_LENGTH} letters while loading SFont, got #{i}" if detect_x >= font_surface.w
+        raise Tea::Error, "Expected #{SFONT_LENGTH} letters while loading SFont, got #{i}" if detect_x >= font_surface.w
 
         char_right_x = detect_x
         char_right_x += 1 while Tea::Color.mix(*font_surface.get_rgba(font_surface[char_right_x, 0])) != Tea::Color::MAGENTA && char_right_x < font_surface.w
         char_w = char_right_x - detect_x
         char_h = font_surface.h
+        trans_rgba = transparent_color ? Color.split(transparent_color) : nil
 
-        g = Bitmap.new(char_w, char_h, Color::CLEAR)
+        glyph = Bitmap.new(char_w, char_h, Color::CLEAR)
         for gy in 0...char_h
           for gx in 0...char_w
-            g[gx, gy] = Color.mix(*font_surface.get_rgba(font_surface[detect_x + gx, gy]))
+            glyph[gx, gy] = Color.mix(*font_surface.get_rgba(font_surface[detect_x + gx, gy]))
           end
         end
 
         detect_x = char_right_x
 
-        g
+        glyph
       end
 
       glyphs
     end
+
+    # Run a block for each code point of the string.
+    def each_code_point(string)
+      if ruby_version_match = RUBY_VERSION.match(/(\d+)\.(\d+)\.\d+/)
+        ruby_major = ruby_version_match[1].to_i
+        ruby_minor = ruby_version_match[2].to_i
+        if ruby_major >= 1 && ruby_minor >= 9
+          string.codepoints do |pt|
+            yield pt
+          end
+        else
+          string.length.times do |i|
+            pt = string[i]
+            yield pt
+          end
+        end
+      end
+    end
+
+    # Get the designated width of the glyph associated with the code point.
+    def get_glyph_w(code_point)
+      case @font_type
+      when BITMAP_FONT
+        @glyphs[code_point].w
+      when SFONT
+        if SFONT_START <= code_point && code_point < SFONT_START + SFONT_LENGTH
+          @glyphs[code_point - SFONT_START].w
+        elsif code_point == CODE_POINT_SPACE
+          # Substitute with first char width, like SFont reference library.
+          @glyphs[0].w
+        else
+          0
+        end
+      end
+    end
+
+    # Get the glyph bitmap for the given code point.  May return nil if the
+    # code point doesn't map to any glyph.
+    def get_glyph(code_point)
+      case @font_type
+      when BITMAP_FONT
+        @glyphs[code_point]
+      when SFONT
+        if SFONT_START <= code_point && code_point < SFONT_START + SFONT_LENGTH
+          @glyphs[code_point - SFONT_START]
+        else
+          nil
+        end
+      end
+    end
+
 
   end
 
